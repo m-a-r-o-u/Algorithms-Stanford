40bbabf24d2f682c89680070471d57f3376cca87
Mares.Barekzai <Mares.Barekzai@posteo.de>
2021-02-07 18:56:03 +0100

    Improving solution and adding karatsuba

--- 1 Divide and Conquer, Sorting and Searching, and Randomized Algorithms/Week 1/1wk1_a1.py
+++ 1 Divide and Conquer, Sorting and Searching, and Randomized Algorithms/Week 1/1wk1_a1.py
@@ -1,19 +1,39 @@
-"""
-Recursive integer multiplication
-"""
+def multi(x,y):
+    '''
+    Recursive algorithm to multiply two integers
+    '''
 
-
-def multi(num1, num2):
-    n1, n2 = len(num1), len(num2)
-    a, b, c, d = num1[:n1//2], num1[n1//2:], num2[:n2//2], num2[n2//2:]
-    if min(len(a), len(b), len(c), len(d)) == 1:
-        ac = str(int(a)*int(c))
-        ad = str(int(a)*int(d))
-        bc = str(int(b)*int(c))
-        bd = str(int(b)*int(d))
+    if len(str(x)) == 1 or len(str(y)) == 1:
+        return x * y
     else:
+        m = max(len(str(x)), len(str(y))) // 2
+
+        a, b = x // 10**m, x % 10**m
+        c, d = y // 10**m, y % 10**m
+
         ac = multi(a, c)
         ad = multi(a, d)
         bc = multi(b, c)
         bd = multi(b, d)
-    return str(int(ac)*int(10**(len(a)*2))+(int(ad)+int(bc))*int(10**len(a))+int(bd))
+
+        return ac * 10**(2*m) + (ad + bc) * 10**m + bd
+
+
+def karat(x,y):
+    '''
+    Karatsuba algorithm to multiply two integers recursively
+    '''
+
+    if len(str(x)) == 1 or len(str(y)) == 1:
+        return x * y
+    else:
+        m = max(len(str(x)), len(str(y))) // 2
+
+        a, b = x // 10**m, x % 10**m
+        c, d = y // 10**m, y % 10**m
+
+        ac = karat(a, c)
+        bd = karat(b, d)
+        zz = karat((a+b), (c+d))
+
+        return ac * 10**(2*m) + (zz - ac - bd) * 10**m + bd
