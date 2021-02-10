3cf541dad8a6a902bab86eb88d769fc729bc913a
Mares.Barekzai <Mares.Barekzai@posteo.de>
2021-02-10 17:13:41 +0100

    Adding Quick Sort

--- 1_divide_and_conquer_sorting_and_searching_and_randomized_algorithms/3_week/1wk3_a1.py
+++ 1_divide_and_conquer_sorting_and_searching_and_randomized_algorithms/3_week/1wk3_a1.py
@@ -1,63 +1,32 @@
+#!/usr/bin/env python
 """
 QuickSort with different pivot
 """
 
-ia = []
-f = open('QuickSort.txt', 'r')
-ls = f.readlines()
-f.close()
-ia = [int(i) for i in ls]
-
-
-def QS1(a):
-    if len(a) <= 1:
-        return (a, 0)
-    pivot = a[0]
-    i, j = 1, 1
-    n = len(a)-1
-    while j < len(a):
-        if a[j] > pivot:
-            j += 1
-        else:
-            a[i], a[j] = a[j], a[i]
-            i += 1
-            j += 1
-    a[0], a[i-1] = a[i-1], a[0]
-    a[:i-1], nleft = QS1(a[:i-1])
-    a[i:], nright = QS1(a[i:])
-    return (a, n+nleft+nright)
-
-
-def QS2(a):
-    if len(a) <= 1:
-        return (a, 0)
-    pivot = a[-1]
-    a[0], a[-1] = a[-1], a[0]
-    i, j = 1, 1
-    n = len(a)-1
-    while j < len(a):
-        if a[j] > pivot:
-            j += 1
-        else:
-            a[i], a[j] = a[j], a[i]
-            i += 1
-            j += 1
-    a[0], a[i-1] = a[i-1], a[0]
-    a[:i-1], nleft = QS2(a[:i-1])
-    a[i:], nright = QS2(a[i:])
-    return (a, n+nleft+nright)
-
-
-def QS3(a):
-    if len(a) <= 1:
-        return (a, 0)
+def elect(a):
     findpivot = [a[0], a[(len(a)-1)//2], a[-1]]
     k = findpivot.copy()
     k.remove(min(k))
     knum = findpivot.index(min(k))
     pivotnum = 0 if knum == 0 else (len(a)-1)//2 if knum == 1 else len(a)-1
-    pivot = a[pivotnum]
     a[0], a[pivotnum] = a[pivotnum], a[0]
+    return a[0]
+
+def final(a):
+    a[0], a[-1] = a[-1], a[0]
+    return a[0]
+
+def first(a):
+    return a[0]
+
+select_pivot = {'first':first, 'final':final, 'elect':elect}
+
+def quick_sort(a, method):
+    if len(a) <= 1:
+        return (a, 0)
+
+    pivot = select_pivot[method](a)
+
     i, j = 1, 1
     n = len(a)-1
     while j < len(a):
@@ -67,13 +36,19 @@ def QS3(a):
             a[i], a[j] = a[j], a[i]
             i += 1
             j += 1
+
     a[0], a[i-1] = a[i-1], a[0]
-    a[:i-1], nleft = QS3(a[:i-1])
-    a[i:], nright = QS3(a[i:])
-    return (a, n+nleft+nright)
+    a[:i-1], l = quick_sort(a[:i-1], method)
+    a[i:], r = quick_sort(a[i:], method)
+    return (a, n + l + r)
 
 
-ia1, ia2, ia3 = ia.copy(), ia.copy(), ia.copy()
-ia1, num1 = QS1(ia1)
-ia2, num2 = QS2(ia2)
-ia3, num3 = QS3(ia3)
+if __name__ == "__main__":
+    with open('data.txt', 'r') as f:
+        data = [int(i) for i in f.readlines()]
+
+    d1, n1 = quick_sort(data.copy(), 'first')
+    d2, n2 = quick_sort(data.copy(), 'final')
+    d3, n3 = quick_sort(data.copy(), 'elect')
+
+    print(f'first:{n1}, final:{n2}, advanced:{n3}')
