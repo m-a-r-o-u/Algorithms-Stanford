9c32a46f25efbf1a14ac2b33e67863d90589198d
Mares.Barekzai <Mares.Barekzai@posteo.de>
2021-02-11 17:16:28 +0100

    Adapting the Karger Algorithm

--- 1_divide_and_conquer_sorting_and_searching_and_randomized_algorithms/4_week/1wk4_a1.py
+++ 1_divide_and_conquer_sorting_and_searching_and_randomized_algorithms/4_week/1wk4_a1.py
@@ -1,42 +1,43 @@
-"""
-Min Cut
-"""
+#!/usr/bin/env python
 
-from random import choice
-
-
-f = open('kargerMinCut.txt', 'r')
-ls = f.readlines()
-f.close()
-graph = [list(map(int, i.split('\t')[:-1])) for i in ls]
-
-
-def create():
-    global graph
-    return [i.copy() for i in graph]
+import copy
+import random
 
 
 def mincut(g):
+    '''
+    Karger Algorithm: Estimate the graph split with the minimum number of cuts
+    '''
     while len(g) > 2:
-        c1 = choice(range(len(g)))
-        v_del = g.pop(c1)
-        c2 = choice(range(1, len(v_del)))
-        v1, v2 = v_del[0], v_del[c2]
+        '''Choose a random node'''
+        _ = random.choice(range(len(g)))
+        v_del = g.pop(_)
+        _ = random.choice(range(1, len(v_del)))
+        v1, v2 = v_del[0], v_del[_]
+
         while v2 in v_del:
             v_del.remove(v2)
+
         for i in range(len(g)):
             if g[i][0] == v2:
                 g[i] += v_del
                 while v1 in g[i]:
                     g[i].remove(v1)
             for j in range(len(g[i])):
-                g[i][j] = v2 if g[i][j] == v1 else g[i][j]
-    return len(g[0])-1
+                if g[i][j] == v1:
+                    g[i][j] = v2
+    return len(g[0]) - 1
 
 
-N = 1000
-cut = []
-for i in range(N):
-    cut += [mincut(create())]
+if __name__ == '__main__':
+    with open('data.txt', 'r') as f:
+        data = [list(map(int, i.split('\t')[:-1])) for i in f.readlines()]
 
-print(min(cut))
+    cuts = []
+    for i in range(1000):
+        cuts += [mincut(copy.deepcopy(data))]
+        minimum = min(cuts)
+        if not i % 50:
+            print(f'Split: {i:4d}, Minimum: {minimum}')
+
+    print(f'Final Estimated Minimum: {minimum}')
